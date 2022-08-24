56839f43f4543e46309386167df090e3f013068e
Rhomeinel <rhomeinel@gmail.com>
2022-08-24 03:46:12 -0700

    Fix trailing whitespace and aad description

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,9 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
  *
- * Return: whatever 
+ * Return: Pointer to the newly created node
+ *         NULL on failure
+ *         NULL if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
